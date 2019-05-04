ab5d40c83a8ad26fa379bd02ed285aca97cc2986
MarletBaptiste <49087437+MarletBaptiste@users.noreply.github.com>
2019-05-04 16:11:57 +0200

    Update HeuristiqueEscampe.java

--- escampe/HeuristiqueEscampe.java
+++ escampe/HeuristiqueEscampe.java
@@ -34,114 +34,147 @@ public class HeuristiqueEscampe implements iia.jeux.alg.Heuristique {
 		if (j.bool) {
 
 			// si je suis mis en echec par une pièce
-			if (!j.bool) {
-				for (Piece piece : p2.pieces) {
-					if (piece.player == false) {
-						for (Coup coup : piece.possibleMoves()) {
-							if (coup != null && piece.possibleMoves().length != 0 && coup.toX == piece.x
-									&& coup.toY == piece.y && piece.licorne) {
-								return Integer.MIN_VALUE + 10;
-							}
+			int xLicorne1 = -1;
+			int yLicorne1 =-1;
+			for (Piece piece : p2.pieces) {
+				if(piece.player==false || piece.player && piece.licorne) {
+					for (Coup coup : piece.possibleMoves()) {
+						if(piece.licorne && piece.player) {
+							xLicorne1 = piece.x;
+							yLicorne1 = piece.y;
+						}
+						if (coup != null && piece.possibleMoves().length != 0 && coup.toX == xLicorne1
+								&& coup.toY == yLicorne1 && piece.player==false) {
+							System.out.println("JE SUIS MIS EN ECHEC(je suis noir)!!!!!!!!");
+							points=points-200; 
+
+
 						}
 					}
 				}
 			}
+
 			// si je met la licorne adverse en echec
-			if (!j.bool) {
-				for (Piece piece : p2.pieces) {
-					if (piece.player == true) {
-						for (Coup coup : piece.possibleMoves()) {
-							if (coup != null && piece.possibleMoves().length != 0 && coup.toX == piece.x
-									&& coup.toY == piece.y && piece.licorne) {
-								points = points + 60;
-							}
-						}
+			int xLicorne = -1;
+			int yLicorne =-1;
+			for (Piece piece : p2.pieces) {
+
+				for (Coup coup : piece.possibleMoves()) {
+
+					if(piece.licorne && piece.player==false) {
+						xLicorne = piece.x;
+						yLicorne = piece.y;
+					}
+					if (coup != null && piece.possibleMoves().length != 0 && coup.toX == xLicorne
+							&& coup.toY == yLicorne && piece.player==true) {
+						points = points + 60;
+						System.out.println("J'AI MIS UNE LICORNE BLANCHE EN ECHEC!!!!!");
 					}
 				}
-			} else {
-				if (j.bool) {
-					// chaque coup possible rapporte des points
-					for (Piece piece : p2.pieces) {
-						if (j.bool && piece.licorne == false && piece.player == true) {
-							for (Coup coup : piece.possibleMoves()) {
-								points = points + 10;
-							}
-						}
-					}
-					// si je controle les cases du milieu
-					for (int i = 0; i < 6; i++) {
-						for (int v = 0; v < 6; v++) {
-							if (p2.array[2][2].getPiece() != null && p2.array[2][2].getPiece().licorne
-									|| p2.array[2][3].getPiece() != null && !p2.array[2][3].getPiece().licorne
-									|| p2.array[3][2].getPiece() != null && !p2.array[3][2].getPiece().licorne
-									|| p2.array[3][3].getPiece() != null && !p2.array[3][3].getPiece().licorne) {
-								points = points + 30;
-							}
-						}
+
+			}
+
+
+
+			// chaque coup possible rapporte des points
+			for (Piece piece : p2.pieces) {
+				if (j.bool && piece.licorne == false && piece.player == true) {
+					for (Coup coup : piece.possibleMoves()) {
+						points = points + 10;
+
 					}
 				}
 			}
+			// si je controle les cases du milieu
+			for (int i = 0; i < 6; i++) {
+				for (int v = 0; v < 6; v++) {
+					if (p2.array[2][2].getPiece() != null && p2.array[2][2].getPiece().licorne
+							|| p2.array[2][3].getPiece() != null && !p2.array[2][3].getPiece().licorne
+							|| p2.array[3][2].getPiece() != null && !p2.array[3][2].getPiece().licorne
+							|| p2.array[3][3].getPiece() != null && !p2.array[3][3].getPiece().licorne) {
+						points = points + 30;
+					}
+				}
+			}
+
 			return points;
-		} else {
+		} if(j.bool==false){
+
+
 			// si je suis mis en echec par une pièce
-			if (!j.bool) {
-				for (Piece piece : p2.pieces) {
-					if (piece.player == true) {
-						for (Coup coup : piece.possibleMoves()) {
-							if (coup != null && piece.possibleMoves().length != 0 && coup.toX == piece.x
-									&& coup.toY == piece.y && piece.licorne) {
-								return Integer.MIN_VALUE + 10;
-							}
-						}
+			int xLicorne2 = -1;
+			int yLicorne2 =-1;
+			for (Piece piece : p2.pieces) {
+				for (Coup coup : piece.possibleMoves()) {
+					if(piece.licorne && piece.player==false) {
+						xLicorne2 = piece.x;
+						yLicorne2 = piece.y;
+					}
+					if (coup != null && piece.possibleMoves().length != 0 && coup.toX == xLicorne2
+							&& coup.toY == yLicorne2 && piece.player==true) {
+						System.out.println("JE SUIS MIS EN ECHEC(je suis blanc)!!!!!!!!");
+						points=points-200;
+
+
+					}
+				}
+
+			}
+
+			// si je met la licorne adverse en echec
+			int xLicorne = -1;
+			int yLicorne =-1;
+			for (Piece piece : p2.pieces) {
+
+				for (Coup coup : piece.possibleMoves()) {
+
+					if(piece.licorne && piece.player==true) {
+						xLicorne = piece.x;
+						yLicorne = piece.y;
+					}
+					if (coup != null && piece.possibleMoves().length != 0 && coup.toX == xLicorne
+							&& coup.toY == yLicorne && piece.player==false) {
+						points = points + 60;
+						System.out.println("J'AI MIS UNE LICORNE NOIR EN ECHEC!!!!!");
+					}
+				}
+
+			}
+
+
+			// chaque coup possible rapporte des points
+			for (Piece piece : p2.pieces) {
+				if (!j.bool && piece.licorne == false && piece.player == false) {
+					for (Coup coup : piece.possibleMoves()) {
+						points = points + 10;
 					}
 				}
 			}
-			// si je met la licorne adverse en echec
-			if (!j.bool) {
-				for (Piece piece : p2.pieces) {
-					if (piece.player == false) {
-						for (Coup coup : piece.possibleMoves()) {
-							if (coup != null && piece.possibleMoves().length != 0 && coup.toX == piece.x
-									&& coup.toY == piece.y && piece.licorne) {
-								points = points + 60;
-							}
+
+			// si je controle les cases du milieu
+			for (int i = 0; i < 6; i++) {
+				for (int v = 0; v < 6; v++) {
+
+					if (p2.array[v][i].getPiece() != null) {
+						if (p2.array[2][2].getPiece() != null && p2.array[2][2].getPiece().licorne
+								|| p2.array[2][3].getPiece() != null && !p2.array[2][3].getPiece().licorne
+								|| p2.array[3][2].getPiece() != null && !p2.array[3][2].getPiece().licorne
+								|| p2.array[3][3].getPiece() != null && !p2.array[3][3].getPiece().licorne) {
+							points = points + 30;
 						}
+
 					}
 				}
-			} else {
-				if (!j.bool) {
-					// chaque coup possible rapporte des points
-					for (Piece piece : p2.pieces) {
-						if (!j.bool && piece.licorne == false && piece.player == false) {
-							for (Coup coup : piece.possibleMoves()) {
-								points = points + 10;
-							}
-						}
-					}
-
-					// si je controle les cases du milieu
-					for (int i = 0; i < 6; i++) {
-						for (int v = 0; v < 6; v++) {
-
-							if (p2.array[v][i].getPiece() != null) {
-								if (p2.array[2][2].getPiece() != null && p2.array[2][2].getPiece().licorne
-										|| p2.array[2][3].getPiece() != null && !p2.array[2][3].getPiece().licorne
-										|| p2.array[3][2].getPiece() != null && !p2.array[3][2].getPiece().licorne
-										|| p2.array[3][3].getPiece() != null && !p2.array[3][3].getPiece().licorne) {
-									points = points + 30;
-								}
-
-							}
-						}
-					}
-
-				}
-
 			}
 			return points;
 		}
+
+		return points;
 	}
 
-
-
 }
+
+
+
+
+
